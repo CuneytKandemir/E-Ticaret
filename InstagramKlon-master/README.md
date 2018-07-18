068f2b600e2fd656edbe8cb700e99946f7c9dc77
Cuneyt Kandemir <41365438+CuneytKandemir@users.noreply.github.com>
2018-07-18 19:26:25 +0300

    Revert "update"
    This reverts commit d81a1117e148be4438408a1888b08d32e9a16b9f.

--- InstagramKlon-master/README.md
+++
@@ -1,1 +0,0 @@
-Bitirme Projesi için yazılmış bir instagram klonudur. Veritabanı olarak firebase kullanılmıştır. Platform Androiddir.
--- InstagramKlon-master/instagramcc/app/src/main/java/com/abisininstagram/instagramcc/instagramcc/Likes/RequestFragment.java
+++
@@ -1,8 +0,0 @@
-package com.abisininstagram.instagramcc.instagramcc.Likes;
-
-/**
- * Created by Camokan on 30.11.2017.
- */
-
-public class RequestFragment {
-}
